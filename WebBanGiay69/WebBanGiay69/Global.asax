170edd1266471e4fef7d4085d114212035f7b810
quoctrinhtrong69 <connguoiao69@gmail.com>
2017-12-25 15:47:36 +0700

    Tao project moi

---
+++ WebBanGiay69/WebBanGiay69/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebBanGiay69.MvcApplication" Language="C#" %>
---
+++ WebBanGiay69/WebBanGiay69/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBanGiay69.Startup))]
+namespace WebBanGiay69
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
